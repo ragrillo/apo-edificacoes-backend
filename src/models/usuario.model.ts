dc383484f99bcc9b8b8aa9cf34789c7fcec31484
Edison Matias <devedisonmatias@gmail.com>
2023-02-03 16:26:54 -0300

    feat: update usuario model attributes

--- src/models/usuario.model.ts
+++ src/models/usuario.model.ts
@@ -1,6 +1,17 @@
+type Edificacao = 'Escola' | 'UBS' | 'Residência';
+type Status = 'Pendente' | 'Ativado' | 'Desativado';
+
 export interface Usuario {
   id: string;
+  cargo: number;
+  edificacao: Edificacao;
   nomeCompleto: string;
+  telefone: string;
   email: string;
   senha: string;
+  cnpj?: string;
+  razaoSocial?: string;
+  emailEmpresarial?: string;
+  telefoneEmpresarial?: string;
+  status: Status;
 }
--- src/repositories/usuario/create.repository.ts
+++ src/repositories/usuario/create.repository.ts
@@ -2,9 +2,16 @@ import { MongoClient } from '../../database/mongo.database';
 import { Usuario } from '../../models/usuario.model';
 
 export interface CreateUsuarioParams {
+  cargo: string;
+  edificacao: string;
   nomeCompleto: string;
+  telefone: string;
   email: string;
   senha: string;
+  cnpj?: string;
+  razaoSocial?: string;
+  emailEmpresarial?: string;
+  telefoneEmpresarial?: string;
 }
 
 export interface ICreateUsuarioRepository {
@@ -13,7 +20,7 @@ export interface ICreateUsuarioRepository {
 
 export class CreateUsuarioRepository implements ICreateUsuarioRepository {
   async handle(data: CreateUsuarioParams): Promise<Usuario> {
-    const { insertedId } = await MongoClient.db.collection('usuarios').insertOne(data);
+    const { insertedId } = await MongoClient.db.collection('usuarios').insertOne({ ...data, status: 'Pendente' });
     const usuario = await MongoClient.db.collection<Omit<Usuario, 'id'>>('usuarios').findOne({ _id: insertedId });
 
     if (!usuario) {
