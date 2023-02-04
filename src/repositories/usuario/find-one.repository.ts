4057d6f01813c5dab4c6f3ad3727efbb408cddc4
Edison Matias <devedisonmatias@gmail.com>
2023-02-04 10:32:26 -0300

    feat: add findByEmail function to find-one-usuario-repository

--- src/repositories/usuario/find-one.repository.ts
+++ src/repositories/usuario/find-one.repository.ts
@@ -3,12 +3,23 @@ import { MongoClient } from '../../database/mongo.database';
 import { Usuario } from '../../models/usuario.model';
 
 export interface IFindOneUsuarioRepository {
-  handle(id: string): Promise<Usuario | null>;
+  findById(id: string): Promise<Usuario | null>;
+  findByEmail(email: string): Promise<Usuario | null>;
 }
 
 export class FindOneUsuarioRepository implements IFindOneUsuarioRepository {
-  async handle(id: string): Promise<Usuario | null> {
+  async findById(id: string): Promise<Usuario | null> {
     const usuario = await MongoClient.db.collection<Omit<Usuario, 'id'>>('usuarios').findOne({ _id: new ObjectId(id) });
+
+    if (!usuario) {
+      return null;
+    }
+
+    return { id: usuario._id.toHexString(), ...usuario };
+  }
+
+  async findByEmail(email: string): Promise<Usuario | null> {
+    const usuario = await MongoClient.db.collection<Omit<Usuario, 'id'>>('usuarios').findOne({ email });
 
     if (!usuario) {
       return null;
