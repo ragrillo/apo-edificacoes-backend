2e6b8d2ff65874137b13016c4d146c1992a62f9e
Edison Matias <devedisonmatias@gmail.com>
2023-02-06 05:18:18 -0300

    feat: ensure separate development and production environment variables

--- src/database/mongo.database.ts
+++ src/database/mongo.database.ts
@@ -1,18 +1,21 @@
+import { config } from 'dotenv';
+import { resolve } from 'path';
 import { Db, MongoClient as Mongo } from 'mongodb';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+config({ path: resolve(__dirname, `../../.env.${NODE_ENV}`) });
 
 export const MongoClient = {
   client: undefined as unknown as Mongo,
   db: undefined as unknown as Db,
 
   async connect(): Promise<void> {
-    const url = 'mongodb://localhost:27017';
-    const database = 'apo-edificacoes-db';
-    const username = 'root';
-    const password = 'root';
+    const { MONGODB_URL, MONGODB_DATABASE } = process.env;
 
-    const client = await Mongo.connect(url, {
-      auth: { username, password },
-    });
+    const url = MONGODB_URL || 'mongodb://localhost:27017';
+    const database = MONGODB_DATABASE || 'apo-edificacoes-dev';
+
+    const client = await Mongo.connect(url);
 
     this.client = client;
     this.db = client.db(database);
