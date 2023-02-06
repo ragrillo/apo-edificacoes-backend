b5ffa9409d69f1e4cd8b86c98c5f6a0331add8b3
Edison Matias <devedisonmatias@gmail.com>
2023-02-06 04:48:21 -0300

    feat: ensure json-to-csv adapter read all formulario data

--- src/adapters/json-to-csv.adapter.ts
+++ src/adapters/json-to-csv.adapter.ts
@@ -6,10 +6,9 @@ export interface IJsonToCsvAdapter {
 
 export class JsonToCsvAdapter implements IJsonToCsvAdapter {
   async handle(formulario: Formulario[]): Promise<Buffer> {
-    let cabecalho = Object.keys(formulario[0].respostas[0]).join(',').concat('\n');
-    let dados = formulario.map((row) => row.respostas[0]);
-    let corpo = dados.map((row) => Object.values(row).join(',')).join('\n');
+    let cabecalho = Object.keys(formulario[0].respostas[0]).join(',').toUpperCase().concat('\n');
+    let dados = formulario.map((row) => row.respostas.map((row) => Object.values(row).join(',')).join('\n')).join('\n');
 
-    return Buffer.from(cabecalho.concat(corpo));
+    return Buffer.from(cabecalho.concat(dados));
   }
 }
