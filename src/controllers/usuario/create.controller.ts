80a145f57661008fb9b9d80228ed970cece40b5a
Edison Matias <devedisonmatias@gmail.com>
2023-02-08 15:04:54 -0300

    refactor: send message in create usuario http response

--- src/controllers/usuario/create.controller.ts
+++ src/controllers/usuario/create.controller.ts
@@ -1,18 +1,17 @@
 import { HttpRequest } from '../../interfaces/http-request';
 import { HttpResponse } from '../../interfaces/http-response';
-import { Usuario } from '../../models/usuario.model';
 import { CreateUsuarioParams } from '../../repositories/usuario/create.repository';
 import { ICreateUsuarioService } from '../../services/usuario/create.service';
 import { HttpStatusCodes } from '../../utils/http-status-codes.utils';
 
 export interface ICreateUsuarioController {
-  handle(data: HttpRequest<CreateUsuarioParams>): Promise<HttpResponse<Usuario>>;
+  handle(data: HttpRequest<CreateUsuarioParams>): Promise<HttpResponse<string>>;
 }
 
 export class CreateUsuarioController implements ICreateUsuarioController {
   constructor(private readonly createUsuarioService: ICreateUsuarioService) { }
 
-  async handle(data: HttpRequest<CreateUsuarioParams>): Promise<HttpResponse<Usuario>> {
+  async handle(data: HttpRequest<CreateUsuarioParams>): Promise<HttpResponse<string>> {
     try {
       if (!data.body) {
         return {
@@ -21,11 +20,11 @@ export class CreateUsuarioController implements ICreateUsuarioController {
         };
       }
 
-      const usuario = await this.createUsuarioService.handle(data.body);
+      await this.createUsuarioService.handle(data.body);
 
       return {
         statusCode: HttpStatusCodes.CREATED,
-        body: usuario,
+        body: 'Cadastro realizado com sucesso! Aguarde a aprovação do administrador.',
       };
     } catch (error) {
       console.error(error);
