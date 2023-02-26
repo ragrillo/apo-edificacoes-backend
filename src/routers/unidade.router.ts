b57bd0341ee63b0fa4171b067028e6a425d4e040
Edison Matias <devedisonmatias@gmail.com>
2023-02-26 10:50:19 -0300

    refactor: unidade router

--- src/routers/unidade.router.ts
+++ src/routers/unidade.router.ts
@@ -1,87 +1,83 @@
 import { Router, Request, Response } from 'express';
 
-import { ResidenciaController } from '../controllers/residencia.controller';
+import { IResidenciaController, ResidenciaController } from '../controllers/residencia.controller';
 import { ResidenciaMongoRepository } from '../repositories/residencia.repository';
 import { ResidenciaService } from '../services/residencia.service';
-import { EscolaController } from '../controllers/escola.controller';
+import { EscolaController, IEscolaController } from '../controllers/escola.controller';
 import { EscolaMongoRepository } from '../repositories/escola.repository';
 import { EscolaService } from '../services/escola.service';
 import { UBSMongoRepository } from '../repositories/ubs.repository';
 import { UBSService } from '../services/ubs.service';
-import { UBSController } from '../controllers/ubs.controller';
-
-function makeEscolaController() {
-  const escolaRepository = new EscolaMongoRepository();
-  const escolaService = new EscolaService(escolaRepository);
-  const escolaController = new EscolaController(escolaService);
-
-  return escolaController;
-}
-
-function makeResidenciaController() {
-  const residenciaRepository = new ResidenciaMongoRepository();
-  const residenciaService = new ResidenciaService(residenciaRepository);
-  const residenciaController = new ResidenciaController(residenciaService);
-
-  return residenciaController;
-}
-
-function makeUBSController() {
-  const ubsRepository = new UBSMongoRepository();
-  const ubsService = new UBSService(ubsRepository);
-  const ubsController = new UBSController(ubsService);
-
-  return ubsController;
-}
+import { IUBSController, UBSController } from '../controllers/ubs.controller';
 
 class UnidadeRouter {
-  router: Router;
+  public router: Router;
+  private escolaController: IEscolaController;
+  private residenciaController: IResidenciaController;
+  private ubsController: IUBSController;
 
   constructor() {
     this.router = Router();
+
+    this.escolaController = this.makeEscolaController();
+    this.residenciaController = this.makeResidenciaController();
+    this.ubsController = this.makeUBSController();
+
     this.routes();
   }
 
-  routes() {
-    const escolaController = makeEscolaController();
-    
-    this.router.get('/escolas', async (request: Request, response: Response) => {
-      const { statusCode, body } = await escolaController.findAll();
+  private makeEscolaController(): IEscolaController {
+    const escolaRepository = new EscolaMongoRepository();
+    const escolaService = new EscolaService(escolaRepository);
+    const escolaController = new EscolaController(escolaService);
 
+    return escolaController;
+  }
+
+  private makeResidenciaController(): IResidenciaController {
+    const residenciaRepository = new ResidenciaMongoRepository();
+    const residenciaService = new ResidenciaService(residenciaRepository);
+    const residenciaController = new ResidenciaController(residenciaService);
+
+    return residenciaController;
+  }
+
+  private makeUBSController(): IUBSController {
+    const ubsRepository = new UBSMongoRepository();
+    const ubsService = new UBSService(ubsRepository);
+    const ubsController = new UBSController(ubsService);
+
+    return ubsController;
+  }
+
+  private routes(): void {
+    this.router.get('/escolas', async (request: Request, response: Response) => {
+      const { statusCode, body } = await this.escolaController.findAll();
       return response.status(statusCode).json(body);
     });
 
     this.router.post('/escolas', async (request: Request, response: Response) => {
-      const { statusCode, body } = await escolaController.create(request);
-
+      const { statusCode, body } = await this.escolaController.create(request);
       return response.status(statusCode).json(body);
     });
 
     this.router.get('/residencias', async (request: Request, response: Response) => {
-      const residenciaController = makeResidenciaController();
-      const { statusCode, body } = await residenciaController.findAll();
-
+      const { statusCode, body } = await this.residenciaController.findAll();
       return response.status(statusCode).json(body);
     });
 
     this.router.post('/residencias', async (request: Request, response: Response) => {
-      const residenciaController = makeResidenciaController();
-      const { statusCode, body } = await residenciaController.create(request);
-
+      const { statusCode, body } = await this.residenciaController.create(request);
       return response.status(statusCode).json(body);
     });
 
     this.router.get('/ubs', async (request: Request, response: Response) => {
-      const ubsController = makeUBSController();
-      const { statusCode, body } = await ubsController.findAll();
-
+      const { statusCode, body } = await this.ubsController.findAll();
       return response.status(statusCode).json(body);
     });
 
     this.router.post('/ubs', async (request: Request, response: Response) => {
-      const ubsController = makeUBSController();
-      const { statusCode, body } = await ubsController.create(request);
-
+      const { statusCode, body } = await this.ubsController.create(request);
       return response.status(statusCode).json(body);
     });
   }
