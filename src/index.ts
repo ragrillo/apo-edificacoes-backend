ad24ddbfbd8fd97d7037157252de7f8df434b8e1
Edison Matias <devedisonmatias@gmail.com>
2023-02-26 10:36:58 -0300

    feat: handle database connection error

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,9 @@ const main = async () => {
     const PORT = process.env.PORT || 3000;
 
     app.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`));
-  } catch (error) {
-    console.error('Houve um erro ao iniciar o servidor: ', error);
+  } catch (error: any) {
+    console.error('Houve um erro ao iniciar o servidor!');
+    console.error(`${error.name}: ${error.message}`);
   }
 }
 
