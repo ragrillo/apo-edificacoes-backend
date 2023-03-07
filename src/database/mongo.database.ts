84a67702bae7609125e32baba0dbf376ccfb7f16
Edison Matias <devedisonmatias@gmail.com>
2023-03-07 09:39:46 -0300

    update: get database url by env file

--- src/database/mongo.database.ts
+++ src/database/mongo.database.ts
@@ -1,12 +1,18 @@
+import { config } from 'dotenv';
+import { resolve } from 'path';
 import { Db, MongoClient as Mongo } from 'mongodb';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const environmentFilepath = resolve(__dirname, '..', '..', `.env.${NODE_ENV}`)
+config({ path: environmentFilepath });
 
 const MongoClient = {
   client: undefined as unknown as Mongo,
   db: undefined as unknown as Db,
 
   async connect(): Promise<void> {
-    const url = 'mongodb://localhost:27017';
-    const database = 'apo-edificacoes-dev';
+    const url = String(process.env.MONGODB_URL);
+    const database = process.env.MONGODB_DATABASE;
 
     const client = await Mongo.connect(url);
 
