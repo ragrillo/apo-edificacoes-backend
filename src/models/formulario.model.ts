a32fd2df66ab9dbabc74cf1708564f1f6fd3daf9
Edison Matias <devedisonmatias@gmail.com>
2023-03-16 09:53:53 -0300

    refactor (model): remove useless types

--- src/models/formulario.model.ts
+++ src/models/formulario.model.ts
@@ -1,16 +1,11 @@
-type SimOuNao = 'Sim' | 'Não' | 'Não sei responder' | 'Não se aplica';
-type ExceleteOuPessimo = 'Excelente' | 'Bom' | 'Regular' | 'Ruim' | 'Péssimo' | 'Não se aplica';
-
 type RespostaType = {
-  criterio: string;
-  pergunta: string;
-  resposta: SimOuNao | ExceleteOuPessimo;
+  codigo: string;
+  resposta: string;
 }
 
 interface FormularioModel {
   id: string;
-  titulo: string;
-  numero: string;
+  ambiente: string;
   respostas: RespostaType[];
 }
 
