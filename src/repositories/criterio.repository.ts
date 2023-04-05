0378f77b1c84d486ede9c598bce586138df67766
Edison Matias <devedisonmatias@gmail.com>
2023-04-05 15:10:51 -0300

    feat: sort criterios by codigo

--- src/repositories/criterio.repository.ts
+++ src/repositories/criterio.repository.ts
@@ -8,7 +8,7 @@ interface ICriterioRepository {
 class CriterioMongoRepository implements ICriterioRepository {
   async findAll(number: string): Promise<CriterioModel[]> {
     const query = { codigo: new RegExp(`^${number}`) };
-    const criterios: CriterioModel[] = await MongoClient.db.collection<CriterioModel>('criterios').find(query).toArray();
+    const criterios: CriterioModel[] = await MongoClient.db.collection<CriterioModel>('criterios').find(query).sort({ codigo: 1 }).toArray();
 
     return criterios;
   }
