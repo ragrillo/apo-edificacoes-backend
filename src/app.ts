de878397f67b81ed3846964c80e584cc8f749acd
Edison Matias <devedisonmatias@gmail.com>
2023-04-14 10:31:28 -0300

    refactor: app

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,9 @@ import UsuarioRouter from './routers/usuario.router';
 import CriterioRouter from './routers/criterio.router';
 import DimensaoRouter from './routers/dimensao.router';
 import FormularioRouter from './routers/formulario.router';
-import DimensaoRouter from './routers/dimensao.router';
 
 class App {
   public app: express.Application;
-  private readonly apiVersion: string = 'v1';
 
   constructor() {
     this.app = express();
@@ -33,15 +31,15 @@ class App {
   }
 
   private loadRoutes(): void {
-    const PREFIX = '/api/v1';
+    this.grettings();
 
-    this.app.use(`${PREFIX}/unidades`, new UnidadeRouter().router);
-    this.app.use(`${PREFIX}/ambientes`, new AmbienteRouter().router);
-    this.app.use(`${PREFIX}/empresas`, new EmpresaRouter().router);
-    this.app.use(`${PREFIX}/usuarios`, new UsuarioRouter().router);
-    this.app.use(`${PREFIX}/criterios`, new CriterioRouter().router);
-    this.app.use(`${PREFIX}/formularios`, new FormularioRouter().router);
-    this.app.use(`${PREFIX}/dimensoes`, new DimensaoRouter().router);
+    this.app.use(`/api/v1/unidades`, new UnidadeRouter().router);
+    this.app.use(`/api/v1/ambientes`, new AmbienteRouter().router);
+    this.app.use(`/api/v1/empresas`, new EmpresaRouter().router);
+    this.app.use(`/api/v1/usuarios`, new UsuarioRouter().router);
+    this.app.use(`/api/v1/criterios`, new CriterioRouter().router);
+    this.app.use(`/api/v1/formularios`, new FormularioRouter().router);
+    this.app.use(`/api/v1/dimensoes`, new DimensaoRouter().router);
   }
 }
 
